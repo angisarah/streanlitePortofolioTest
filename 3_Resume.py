673cdc17a6e725364bfd360d8f0ce1b15e011a29
angisarah <54175999+angisarah@users.noreply.github.com>
2023-11-21 13:12:26 +0300

    Update 3_Resume.py

--- 3_Resume.py
+++ 3_Resume.py
@@ -8,7 +8,7 @@ def local_css(file_name):
     with open(file_name) as f:
         st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
         
-local_css("styles/styles_main.css")
+local_css("styles_main.css")
     
 # get the variables from constants.py
 pronoun = info['Pronoun']
