dd4f9d268f8539e9c61517300d4b179efcb84dab
angisarah <54175999+angisarah@users.noreply.github.com>
2023-11-21 13:11:33 +0300

    Update Portfolio.py

--- Portfolio.py
+++ Portfolio.py
@@ -11,7 +11,7 @@ def local_css(file_name):
     with open(file_name) as f:
         st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
         
-local_css("/home/project/streamlit-chatbot-template/styles/styles_main.css")
+local_css("styles_main.css")
 
 # get the variables from constants.py
 pronoun = info['Pronoun']
