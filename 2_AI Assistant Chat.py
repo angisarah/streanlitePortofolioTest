d3602a566157f10c0d0899654533eefd1e9baed7
angisarah <54175999+angisarah@users.noreply.github.com>
2023-11-21 16:36:35 +0300

    Update 2_AI Assistant Chat.py

--- 2_AI Assistant Chat.py
+++ 2_AI Assistant Chat.py
@@ -3,12 +3,12 @@ from utils.constants import *
 import torch
 #from llama_index import *
 
-#from langchain.embeddings import HuggingFaceEmbeddings
-#from llama_index.embeddings import LangchainEmbedding
-#from langchain.embeddings import OpenAIEmbeddings
-#from llama_index import (GPTVectorStoreIndex, SimpleDirectoryReader, LLMPredictor, ServiceContext,)
-#from langchain.embeddings import HuggingFaceInstructEmbeddings
-#from llama_index import GPTVectorStoreIndex
+from langchain.embeddings import HuggingFaceEmbeddings
+from llama_index.embeddings import LangchainEmbedding
+from langchain.embeddings import OpenAIEmbeddings
+from llama_index import (GPTVectorStoreIndex, SimpleDirectoryReader, LLMPredictor, ServiceContext,)
+from langchain.embeddings import HuggingFaceInstructEmbeddings
+from llama_index import GPTVectorStoreIndex
 
 from ibm_watson_machine_learning.foundation_models.extensions.langchain import WatsonxLLM
 from ibm_watson_machine_learning.foundation_models.utils.enums import ModelTypes, DecodingMethods
@@ -120,7 +120,7 @@ with st.spinner("Initiating the AI assistant. Please hold..."):
     )
                                     
     # Hugging Face models can be supported by using LangchainEmbedding to convert text to embedding vector	
-    #embed_model = LangchainEmbedding(embeddings)
+    embed_model = LangchainEmbedding(embeddings)
     
     # ServiceContext: to encapsulate the resources used to create indexes and run queries    
     service_context = ServiceContext.from_defaults(
