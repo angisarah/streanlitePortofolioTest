5b02d566a49c70f1a9e1f488915d21188c4868c8
angisarah <54175999+angisarah@users.noreply.github.com>
2023-11-21 16:32:16 +0300

    Update 2_AI Assistant Chat.py

--- 2_AI Assistant Chat.py
+++ 2_AI Assistant Chat.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from utils.constants import *
 import torch
-#from llama_index.embedings import LangchainEmbedding
+
 from langchain.embeddings import HuggingFaceEmbeddings
 #from llama_index.embeddings import LangchainEmbedding
 from langchain.embeddings import OpenAIEmbeddings
