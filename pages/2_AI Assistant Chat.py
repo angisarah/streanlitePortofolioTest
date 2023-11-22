83b5c4d148ecbbbdc19cb194d9258b17ae22710f
angisarah <54175999+angisarah@users.noreply.github.com>
2023-11-22 11:51:05 +0300

    Update 2_AI Assistant Chat.py

--- pages/2_AI Assistant Chat.py
+++ pages/2_AI Assistant Chat.py
@@ -1,8 +1,12 @@
 import streamlit as st
 from utils.constants import *
 import torch
-from llama_index import (GPTVectorStoreIndex, SimpleDirectoryReader, LLMPredictor, 
-                         ServiceContext, LangchainEmbedding)
+from llama_index.embeddings import LangchainEmbedding
+from langchain.embeddings import OpenAIEmbeddings
+from llama_index import GPTVectorStoreIndex
+from llama_index import SimpleDirectoryReader
+from llama_index import LLMPredictor
+from llama_index import ServiceContext
 from langchain.embeddings import HuggingFaceInstructEmbeddings
 from ibm_watson_machine_learning.foundation_models.extensions.langchain import WatsonxLLM
 from ibm_watson_machine_learning.foundation_models.utils.enums import ModelTypes, DecodingMethods
