01e082f11f1826e67c0202a0d50399e9d122c12d
angisarah <54175999+angisarah@users.noreply.github.com>
2023-11-22 16:30:55 +0300

    Update constants.py

--- utils/constants.py
+++ utils/constants.py
@@ -112,7 +112,7 @@ endorsements = {
 
 embed_rss= {
     'rss':"""<div style="overflow-y: scroll; height:500px; background-color:white;"> <div id="retainable-rss-embed" 
-        data-rss="https://medium.com/@vicky-note"
+        data-rss="https://medium.com/feed/@vicky-note"
         data-maxcols="3" 
         data-layout="grid"
         data-poststyle="inline" 
